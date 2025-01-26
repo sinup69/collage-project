37cef9c0336e38821cffd03b30737f4f3467ecf8
pshycodr <115141578+pshycodr@users.noreply.github.com>
2025-01-26 22:40:18 +0530

    Enhance UI and add landing page route for improved user experience

--- Hospital_management/Hospital_management/urls.py
+++ Hospital_management/Hospital_management/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # Django admin panel
     path('accounts/', include('accounts.urls')),  # URLs for accounts app
     path('patients/', include('patients.urls')),  # URLs for patients app
     path('', include('accounts.urls')),  # Default home page (e.g., login or dashboard)
+
+    path('', views.landing_page, name='landing_page')
 ]
